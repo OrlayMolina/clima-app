52e1457ba16988d8514664b420d2fdc249b9718e
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-01-05 19:55:29 -0500

    Se corrige error al momento de ingresar los valores en los inputs de ciudad y país.

---
+++ WeatherApp/climaapp/src/components/Resultado.jsx
@@ -0,0 +1,18 @@
+import useClima from "../hooks/useClima";
+
+const Resultado = () => {
+
+    const { resultado } = useClima();
+
+    const { name } = resultado;
+
+    return (
+        <div className="contenedor">
+
+            <h2>El Clima es de {name} es:</h2>
+        
+        </div>
+    )
+}
+
+export default Resultado
